e39ad0978e10f0b8c3651de501ddcfd566591e40
Fabricio <101414179+FaArMa@users.noreply.github.com>
2023-10-13 00:32:49 -0300

    Preparados todos los archivos necesarios

---
+++ src/main/java/io/github/faarma/handcuffsmod/HandcuffsMod.java
@@ -0,0 +1,8 @@
+package io.github.faarma.handcuffsmod;
+
+import net.minecraftforge.fml.common.Mod;
+
+@Mod(HandcuffsMod.ModID)
+public class HandcuffsMod {
+    public static final String ModID = "handcuffsmod";
+}
