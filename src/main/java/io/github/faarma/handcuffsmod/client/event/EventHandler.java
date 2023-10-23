36a4bd1ec4301ddd2189f32098abf72f0e602981
Fabricio <101414179+FaArMa@users.noreply.github.com>
2023-10-23 14:47:17 -0300

    Bloquear algunas teclas del teclado y ratón

--- src/main/java/io/github/faarma/handcuffsmod/client/event/EventHandler.java
+++ src/main/java/io/github/faarma/handcuffsmod/client/event/EventHandler.java
@@ -12,10 +12,12 @@ import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.client.event.GuiOpenEvent;
 import net.minecraftforge.client.event.InputEvent.KeyInputEvent;
 import net.minecraftforge.client.event.InputEvent.MouseScrollEvent;
+import net.minecraftforge.client.event.InputEvent.RawMouseEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.Mod.EventBusSubscriber.Bus;
 import net.minecraftforge.fml.common.ObfuscationReflectionHelper;
+import org.lwjgl.glfw.GLFW;
 
 /**
  * Event handler for events that can occur on the client-side.
@@ -24,6 +26,12 @@ import net.minecraftforge.fml.common.ObfuscationReflectionHelper;
  */
 @Mod.EventBusSubscriber(modid = HandcuffsMod.ModID, bus = Bus.FORGE, value = Dist.CLIENT)
 public class EventHandler {
+    /**
+     * The clickCount field is a private field in the KeyBinding class that holds the click count of a key binding.
+     * It is used in the onKeyInputEvent method to cancel specific key actions when a player is handcuffed.
+     */
+    private static Field clickCountField = ObfuscationReflectionHelper.findField(KeyBinding.class, "field_151474_i");
+
     /**
      * Event handler for the GuiOpenEvent event, which occurs when a GUI screen is opened.
      * This method cancels the opening of the InventoryScreen if the player is handcuffed.
@@ -58,6 +66,31 @@ public class EventHandler {
     }
 
     /**
+     * Event handler for the RawMouseEvent, which occurs when a mouse event (like click) is detected.
+     * This method is responsible for handling mouse input when a player is handcuffed, preventing certain actions.
+     *
+     * @param event The RawMouseEvent that occurred.
+     */
+    @SubscribeEvent
+    public static void onRawMouseEvent(RawMouseEvent event) {
+        Minecraft mc = Minecraft.getInstance();
+        GameSettings gs = mc.options;
+        // Yes, if the first is null it means that you are playing (no inventory, no chat, etc.).
+        if (mc.screen != null || !ItemUtils.isPlayerCuffed(mc.player) || event.getAction() != GLFW.GLFW_PRESS) {
+            return;
+        }
+        /*
+         * Mouse buttons that should be ignored if pressed
+         * - Gameplay
+         *   - Attack/Destroy
+         *   - Use Item/Place Block
+         */
+        if (event.getButton() == gs.keyAttack.getKey().getValue() || event.getButton() == gs.keyUse.getKey().getValue()) {
+            event.setCanceled(true);
+        }
+    }
+
+    /**
      * Event handler for the KeyInputEvent, which occurs when a key is pressed or released.
      * This method is responsible for handling key input when a player is handcuffed, preventing certain actions.
      *
@@ -73,24 +106,33 @@ public class EventHandler {
         }
         /*
          * Keys that should be ignored if pressed
-         * - Drop Selected Item
-         * - Pick Block
-         * - Swap Item With Offhand
-         * - Hotbar Slot 1-9
-         * 
-         * XXX Maybe blocking movement and clicks so Pehkui is not needed?
-         * 
+         * - Movement
+         *   - Jump
+         *   - Strafe Left
+         *   - Stafe Right
+         *   - Walk Backwards
+         *   - Walk Forwards
+         * - Gameplay
+         *   - Attack/Destroy
+         *   - Pick Block
+         *   - Use Item/Place Block
+         * - Inventory
+         *   - Drop Selected Item
+         *   - Swap Item With Offhand
+         *   - Hotbar Slot 1-9
          */
-        KeyBinding[] blacklistKeys = { gs.keyDrop, gs.keyPickItem, gs.keySwapOffhand,
-                gs.keyHotbarSlots[0], gs.keyHotbarSlots[1], gs.keyHotbarSlots[2],
-                gs.keyHotbarSlots[3], gs.keyHotbarSlots[4], gs.keyHotbarSlots[5],
-                gs.keyHotbarSlots[6], gs.keyHotbarSlots[7], gs.keyHotbarSlots[8] };
+        KeyBinding[] blacklistKeys = {
+            gs.keyJump, gs.keyLeft, gs.keyRight, gs.keyDown, gs.keyUp,
+            gs.keyAttack, gs.keyPickItem, gs.keyUse,
+            gs.keyDrop, gs.keySwapOffhand,
+            gs.keyHotbarSlots[0], gs.keyHotbarSlots[1], gs.keyHotbarSlots[2],
+            gs.keyHotbarSlots[3], gs.keyHotbarSlots[4], gs.keyHotbarSlots[5],
+            gs.keyHotbarSlots[6], gs.keyHotbarSlots[7], gs.keyHotbarSlots[8]
+        };
         // Attempt to cancel the action of the "blacklistKeys" key.
-        // XXX It may be more convenient to use Access Transformers or to have the Reflection done only once.
         for (KeyBinding keyBinding : blacklistKeys) {
             if (keyBinding.isDown()) {
                 // Effectively, to cancel the pressed button I must change the clickCount...which is private.
-                Field clickCountField = ObfuscationReflectionHelper.findField(KeyBinding.class, "field_151474_i");
                 try {
                     clickCountField.setInt(keyBinding, 0);
                 } catch (IllegalAccessException e) {
